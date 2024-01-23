3b69a9f517623afb0baaecb111e21dea3c4b605b
Denis Lam <denis@netfront.com.au>
2024-01-23 14:40:25 +1100

    Server: Install Apollo Server and setup GraphQL

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+import 'reflect-metadata';
+
+import { ApolloServer } from '@apollo/server';
+import { resolvers } from '@generated/type-graphql';
+import { buildSchema } from 'type-graphql';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { PrismaClient } from '@prisma/client';
+
+interface Context {
+  prisma: PrismaClient;
+}
+
+(async () => {
+  const port = Number(process.env.SERVER_PORT);
+
+  if (isNaN(port)) {
+    throw new Error('Please ensure that SERVER_PORT is correctly configured in your .env file');
+  }
+
+  const schema = await buildSchema({
+    resolvers,
+    validate: false,
+  });
+
+  const apolloServer = new ApolloServer({
+    schema,
+    stopOnTerminationSignals: true,
+  });
+
+  const prisma = new PrismaClient();
+
+  const { url } = await startStandaloneServer(apolloServer, {
+    context: async (): Promise<Context> => ({
+      prisma,
+    }),
+    listen: {
+      port,
+    },
+  });
+
+  console.log(`Apollo server started on ${url}...`);
+})();
